b923a16a520f836bfbdaf9eae9f388c67a97a732
Adrian Matos <matos.adrianpaul@gmail.com>
2017-02-23 17:08:48 +0800

    Generate authentication

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('main', 'MainController@index');
 
 Route::resource('hospital', 'HospitalController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
