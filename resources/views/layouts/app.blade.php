bffcec79e707e6a4cde54c9fd07f591cde3f9743
Adrian Matos <matos.adrianpaul@gmail.com>
2017-02-24 12:49:05 +0800

    Add sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,8 +50,51 @@
       @endif
     </div>
   </nav>
-  @yield('content')
+    <section class="section">
+      <div class="columns">
+        <div class="column is-2">
+          <aside class="menu">
+            <p class="menu-label">
+              General
+            </p>
+            <ul class="menu-list">
+              <li><a>Dashboard</a></li>
+              <li><a>Customers</a></li>
+            </ul>
+            <p class="menu-label">
+              Administration
+            </p>
+            <ul class="menu-list">
+              <li><a>Team Settings</a></li>
+              <li>
+                <a>Manage Your Team</a>
+                <ul>
+                  <li><a>Members</a></li>
+                  <li><a>Plugins</a></li>
+                  <li><a>Add a member</a></li>
+                </ul>
+              </li>
+              <li><a>Invitations</a></li>
+              <li><a>Cloud Storage Environment Settings</a></li>
+              <li><a>Authentication</a></li>
+            </ul>
+            <p class="menu-label">
+              Transactions
+            </p>
+            <ul class="menu-list">
+              <li><a>Payments</a></li>
+              <li><a>Transfers</a></li>
+              <li><a>Balance</a></li>
+            </ul>
+          </aside>
+        </div>
+        <div class="column">
+          @yield('content')
+        </div>
+      </div>
+    </section>
   <!-- Scripts -->
+
   <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
