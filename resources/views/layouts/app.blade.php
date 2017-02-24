1fc064a8e5281bb262c0cbb77a487528f1e2b44c
Adrian Matos <matos.adrianpaul@gmail.com>
2017-02-24 13:10:13 +0800

    Update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,24 +58,14 @@
               General
             </p>
             <ul class="menu-list">
-              <li><a>Dashboard</a></li>
-              <li><a>Customers</a></li>
+              <li><a>Hospitals</a></li>
+              <li><a>Patients</a></li>
             </ul>
             <p class="menu-label">
               Administration
             </p>
             <ul class="menu-list">
-              <li><a>Team Settings</a></li>
-              <li>
-                <a>Manage Your Team</a>
-                <ul>
-                  <li><a>Members</a></li>
-                  <li><a>Plugins</a></li>
-                  <li><a>Add a member</a></li>
-                </ul>
-              </li>
-              <li><a>Invitations</a></li>
-              <li><a>Cloud Storage Environment Settings</a></li>
+              <li><a>Change Password</a></li>
               <li><a>Authentication</a></li>
             </ul>
             <p class="menu-label">
