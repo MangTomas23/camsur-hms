574f0e664f7162162da5894d4f70bef65736e60b
Adrian Matos <matos.adrianpaul@gmail.com>
2017-02-27 08:56:13 +0800

    Add table

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Patient;
 
 class PatientController extends Controller
 {
   public function index() {
-    return view('patient.index');
+    $patients = Patient::all();
+    return view('patient.index', ['patients' => $patients]);
   }
 }
--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -6,4 +6,28 @@
 
 <h1 class="title is-1">Patients</h1>
 
+<table class="table is-striped">
+  <thead>
+    <tr>
+      <th>Last Name</th>
+      <th>First Name</th>
+      <th>Middle Name</th>
+      <th>Hospital</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse($patients as $patient)
+      <tr>
+        <td>{{ $patient->lastname }}</td>
+        <td>{{ $patient->firstname }}</td>
+        <td>{{ $patient->middlename }}</td>
+      </tr>
+    @empty
+      <tr>
+        <td colspan="4">No data.</td>
+      </tr>
+    @endforelse
+  </tbody>
+</table>
+
 @endsection
