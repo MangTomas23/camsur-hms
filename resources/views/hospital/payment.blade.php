f11aef7fd9820255cf40311725455c70f72a161f
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-01 11:10:42 +0800

    Add payments table

--- resources/views/hospital/payment.blade.php
+++ resources/views/hospital/payment.blade.php
@@ -6,4 +6,25 @@
 
 <h3 class="title is-3">Payments</h3>
 
+<table class="table is-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Patient</th>
+      <th>Date</th>
+      <th>Total</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+
 @endsection
