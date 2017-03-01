bd340abd945d6d3e23a32c187ff96b6705a23815
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-01 11:08:07 +0800

    Add nurse table

--- resources/views/hospital/nurse.blade.php
+++ resources/views/hospital/nurse.blade.php
@@ -6,4 +6,25 @@
 
 <h3 class="title is-3">Nurses</h3>
 
+<table class="table is-striped">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Middle Name</th>
+      <th>Last Name</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+
 @endsection
