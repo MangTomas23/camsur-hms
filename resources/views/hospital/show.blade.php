5a6e472638a32b09cab243f21d43b93506e5e2d1
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-01 09:05:25 +0800

    Update menu

--- resources/views/hospital/show.blade.php
+++ resources/views/hospital/show.blade.php
@@ -9,73 +9,35 @@
       {{ $hospital->hospitalid }}
       <span class="tag is-success">{{ $hospital->status }}</span>
     </h1>
-
-    <section>
-      <h2 class="title is-2">Patients</h2>
-      <table class="table is-striped patient">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-            <th>Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse($patients as $patient)
-          <tr data-id="{{ $patient->id }}">
-            <td>{{ $patient->id }}</td>
-            <td>{{ $patient->lastname }}</td>
-            <td>{{ $patient->firstname }}</td>
-            <td>{{ $patient->middlename }}</td>
-            <td>{{ $patient->gender }}</td>
-            <td>{{ $patient->age }}</td>
-            <td>{{ $patient->address }}</td>
-          </tr>
-          @empty
-          <div class="notification is-warning">
-            Empty records!
-          </div>
-          @endforelse
-        </tbody>
-      </table>
-    </section>
-    <nav class="pagination">
-      <a href="{{ $patients->previousPageUrl() }}" class="pagination-previous">Previous</a>
-      <a href="{{ $patients->nextPageUrl() }}" class="pagination-next">Next page</a>
-      <ul class="pagination-list">
-        <li>
-          <a class="pagination-link" href="{{ $patients->url(1) }}">First</a>
-        </li>
-        <li>
-          <span class="pagination-ellipsis">&hellip;</span>
-        </li>
-        <li>
-          <a class="pagination-link" href="{{ $patients->url($patients->lastPage()) }}">Last</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="columns">
+      <div class="column">
+        <div class="box">
+          <a href="#">Patients</a>
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          Inventory
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          Payments
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          Doctors
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          Nurse
+        </div>
+      </div>
+    </div>
   </div>
 
-<div class="modal">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Modal title</p>
-      <button class="delete"></button>
-    </header>
-    <section class="modal-card-body">
-      <!-- Content ... -->
-    </section>
-    <footer class="modal-card-foot">
-      <a class="button is-success">Save changes</a>
-      <a class="button">Cancel</a>
-    </footer>
-  </div>
-</div>
 @endsection
 
 @push('scripts')
