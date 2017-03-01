7b1869a72983a1abcd7c9e2ce4beedc6e7027c9c
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-01 10:24:04 +0800

    Add table

--- resources/views/hospital/inventory.blade.php
+++ resources/views/hospital/inventory.blade.php
@@ -5,5 +5,21 @@
 @section('content')
 <section>
   <h3 class="title is-3">Inventory</h3>
+  <table class="table is-striped">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Category</th>
+        <th>Quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Item 1</td>
+        <td>Cat 1</td>
+        <td>100</td>
+      </tr>
+    </tbody>
+  </table>
 </section>
 @endsection
