a4a011ba8513dc9b9c2f0284166c86e0d0de2aa9
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-01 11:04:51 +0800

    Add table to doctors view

--- resources/views/hospital/doctor.blade.php
+++ resources/views/hospital/doctor.blade.php
@@ -6,4 +6,31 @@
 
 <h3 class="title is-3">Doctors</h3>
 
+<table class="table is-striped">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Middle Name</th>
+      <th>Last Name</th>
+      <th>Rate</th>
+      <th>Status</th>
+      <th>Designation</th>
+      <th>Department</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+
+</table>
+
 @endsection
