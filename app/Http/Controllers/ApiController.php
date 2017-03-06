1296ef7c0c17e6c61f0ace2d649723a6221aee46
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-06 15:27:49 +0800

    Update search

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,10 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Patient;
+use Validator;
 
 class ApiController extends Controller
 {
   public function searchPatient(Request $request) {
-    return Patient::search($request->name)->get();
+    $validator = Validator::make($request->all(), [
+      'name' => 'required'
+    ]);
+
+    if($validator->fails()) {
+      return ['error' => 'Invalid request.'];
+    }
+
+    $patient = Patient::search($request->name)->get();
+
+    if($patient->isEmpty()) {
+      return ['error' => 'No results found.'];
+    }
+
+    return $patient;
   }
 }
