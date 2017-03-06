fe5e5b3d7d806a497ca1afbb8391c8146300fd14
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-06 13:08:05 +0800

    Add api search patient

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Patient;
 
 class ApiController extends Controller
 {
-    //
+  public function searchPatient(Request $request) {
+    return Patient::search($request->name)->get();
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('sync', 'SyncController@sync');
+Route::get('search/patient', 'ApiController@searchPatient');
