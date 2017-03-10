41ca1ae63aee647ce80dc9a4e3ee4032a0ec32ee
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-10 10:39:09 +0800

    Add memo routes

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,4 @@ Route::group(['prefix' => 'database'], function() {
   Route::get('/', 'DatabaseController@index');
 });
 Route::resource('supplier', 'SupplierController');
+Route::resource('memo', 'MemoController');
