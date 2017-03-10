f20c774be42496bd7b6062922e47269d7278d334
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-10 11:01:58 +0800

    Add modal

--- resources/views/memo/index.blade.php
+++ resources/views/memo/index.blade.php
@@ -6,6 +6,8 @@
 
 <h1 class="title is-1">Memos</h1>
 
+<button type="button" class="button is-info modal-button" data-target="#createModal" name="button">Create</button>
+
 <table class="table is-striped">
   <thead>
     <tr>
@@ -18,4 +20,30 @@
 
   </tbody>
 </table>
+
+<div id="createModal" class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Title</p>
+    </header>
+    <section class="modal-card-body">
+      <form class="" action="index.html" method="post">
+        <label class="label">Description</label>
+        <div class="control">
+          <input type="text" name="" value="" class="input">
+        </div>
+        <label class="label">Hospital</label>
+        <div class="control">
+          <div class="select is-fullwidth">
+            <select name="">
+              <option value="all">All</option>
+            </select>
+          </div>
+        </div>
+      </form>
+    </section>
+    <footer class="modal-card-footer"></footer>
+  </div>
+</div>
 @endsection
