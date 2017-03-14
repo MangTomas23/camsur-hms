bec379daec68dc31195acce52abce1dc8377cca8
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-14 16:04:10 +0800

    Append url

--- app/BulletinAttachment.php
+++ app/BulletinAttachment.php
@@ -4,10 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cloudder;
+use App\Cloudinary;
 
 class BulletinAttachment extends Model
 {
+  protected $appends = ['url'];
+
+  public function getUrlAttribute() {
+    return $this->attributes['url'] = Cloudinary::getURL($this->public_id);
+  }
+
   public function showCloud() {
     return Cloudder::show('rktyyfo1v0dzqacl1yjj');
   }
+
 }
