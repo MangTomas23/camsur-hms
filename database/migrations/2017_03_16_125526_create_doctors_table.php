ba324f07a2f92af4d65f86e452417b0f5162bb90
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-16 12:59:20 +0800

    Create Doctor model plus migration

---
+++ database/migrations/2017_03_16_125526_create_doctors_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('lastname');
+            $table->decimal('rate', 8, 2);
+            $table->string('status');
+            $table->string('designation');
+            $table->string('department');
+            $table->string('hospitalid');
+            $table->integer('hospital_id')->unsigned();
+            $table->foreign('hospital_id')->references('id')->on('hospitals1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
