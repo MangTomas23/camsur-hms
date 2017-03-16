dbdf10cd164062b90255e7667af4dc31216efc35
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-16 13:21:49 +0800

    Create Nurse model plus migration

---
+++ database/migrations/2017_03_16_131739_create_nurses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nurses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hospital_id')->unsigned();
+            $table->foreign('hospital_id')->references('id')->on('hospitals');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('middlename');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nurses');
+    }
+}
