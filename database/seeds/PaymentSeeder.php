3a78a897168baf688c56aa212cd423a5f4d4ea95
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-16 13:39:40 +0800

    Add PaymentSeeder class

---
+++ database/seeds/PaymentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PaymentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('payments')->insert([
+        'patient_id' => 1,
+        'date' => '02-03-2017',
+        'total' => 50000,
+        'status' => ''
+      ]);
+    }
+}
