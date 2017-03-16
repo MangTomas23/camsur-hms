370744b2d2b0b07ff22f1e507ac037aab3c4b9e0
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-16 14:22:56 +0800

    Show users table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
   public function index() {
-    return view('user.index');
+    return view('user.index', [
+      'users' => User::all()
+    ]);
   }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,4 +6,24 @@
 
 <h1 class="title is-1">Users</h1>
 
+<section class="section">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($users as $user)
+        <tr>
+          <td>{{ $user->id }}</td>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</section>
 @endsection
