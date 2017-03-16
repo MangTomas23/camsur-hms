956e39a25192400c15c0bc0a6619e6b567e85c7f
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-16 09:52:58 +0800

    Fix chart

--- resources/views/hospital/show.blade.php
+++ resources/views/hospital/show.blade.php
@@ -45,13 +45,48 @@
         </div>
       </div>
     </div>
+    <section class="section">
+      <h2 class="is-title is-2">Stats</h2>
+      <canvas id="patientChart" width="300" height="100"></canvas>
+    </section>
   </div>
 
 @endsection
 
 @push('scripts')
+<script src="/bower_components/chart.js/dist/Chart.min.js"></script>
 <script>
   $(document).ready( function() {
+    var ctx = 'patientChart';
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+          '02/01','02/02','02/03','02/04','02/05','02/06','02/07','02/08',
+          '02/09','02/10','02/11','02/12','02/13'
+        ],
+        datasets: [{
+          label: '# of patients per day. (February)',
+          data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 8, 9, 6, 12 , 4, 10],
+          backgroundColor: [
+              'rgba(99, 255, 115, 0.41)',
+          ],
+          borderColor: [
+              'rgba(70, 207, 76, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+    });
     $('.hospital-menu .box').on('click', function() {
       window.location.href = $(this).find('a').attr('href');
     });
