9ed76a11c12b6b1f645dd2d60ca59a8b21abd0c5
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-17 10:45:36 +0800

    Switch from table to box

--- resources/views/hospital/index.blade.php
+++ resources/views/hospital/index.blade.php
@@ -5,28 +5,20 @@
 @section('content')
 
 <h1 class="title is-1">Hospitals</h1>
-<table class="table is-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Description</th>
-      <th>Status</th>
-    </tr>
-  </thead>
-  <tbody>
 
-    @foreach($hospitals as $hospital)
-      <tr>
-        <td>
-          <a href="/hospital/{{ $hospital->id }}">
-            {{ $hospital->hospitalid }}
-          </a>
-        </td>
-        <td>{{ $hospital->hospitaldescription }}</td>
-        <td>{{ $hospital->status }}</td>
-      </tr>
-    @endforeach
-  </tbody>
-</table>
-
+<section class="section hospital-list is-paddingless">
+  @foreach($hospitals as $hospital)
+    <a class="box" href="{{ route('hospital.show', $hospital->id) }}">
+      <div class="columns">
+        <div class="column">
+          <strong>{{ $hospital->hospitaldescription }}</strong>
+          <p>{{ $hospital->hospitalid }}</p>
+        </div>
+        <div class="column has-text-right">
+          <span class="tag is-medium is-info">{{ $hospital->status }}</span>
+        </div>
+      </div>
+    </a>
+  @endforeach
+</section>
 @endsection
