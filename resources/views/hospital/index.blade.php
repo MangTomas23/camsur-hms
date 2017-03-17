9a4d9943428448a5df90828e5c5149ef528245de
Adrian Matos <matos.adrianpaul@gmail.com>
2017-03-17 11:06:14 +0800

    Add icon

--- resources/views/hospital/index.blade.php
+++ resources/views/hospital/index.blade.php
@@ -11,7 +11,16 @@
 <section class="section hospital-list is-paddingless">
   @foreach($hospitals as $hospital)
     <a class="box" href="{{ route('hospital.show', $hospital->id) }}">
-      <div class="columns">
+      <article class="media">
+        <div class="media-left">
+          <figure class="img is-64x64">
+            <span class="icon is-large">
+              <i class="fa fa-hospital-o"></i>
+            </span>
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="columns">
         <div class="column">
           <strong>{{ $hospital->hospitaldescription }}</strong>
           <p>{{ $hospital->hospitalid }}</p>
@@ -20,13 +29,15 @@
           <span class="tag is-medium is-info">
             <span class="icon is-small">
               <i class="fa fa-dot-circle-o"></i>
-            </span> 
+            </span>
             <span>
               {{ $hospital->status }}
             </span>
           </span>
         </div>
       </div>
+        </div>
+      </article>
     </a>
   @endforeach
 </section>
